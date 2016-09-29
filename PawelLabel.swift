3c0c693d9e43d969ce35d891b87596353e621acf
= <Pawel.Walicki@cognizant.com>
2016-09-29 08:58:56 +0200

    Added PawelLabel class

--- PawelLabel.swift
+++ PawelLabel.swift
@@ -6,4 +6,30 @@
 //
 //
 
-import Foundation
+
+
+import UIKit
+
+public class PawelLabel: UILabel {
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupTheme()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        setupTheme()
+    }
+    
+    // MARK: - Public  methods
+    
+    public func setupTheme(){
+        textColor = UIColor.red
+        
+    }
+}
